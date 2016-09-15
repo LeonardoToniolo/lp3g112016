819eb81b3b4b7478e70b2bfa5a67cba6e83fbda7
31512720 <31512720@087909-P33S103.macklabs.br>
2016-09-15 09:34:34 -0300

    Fist Commit projeto lp3 primeira versão

---
+++ BaseLP3-war/src/java/com/br/lp3/controller/command/PageCommand.java
@@ -0,0 +1,57 @@
+package com.br.lp3.controller.command;
+
+import com.br.lp3.model.dao.Userlp3DAO;
+import com.br.lp3.model.entities.Userinfo;
+import com.br.lp3.model.entities.Userlp3;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 1147106
+ */
+public class PageCommand implements Command {
+
+
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private String responsePage;
+
+
+    @Override
+    public void init(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+    }
+
+    @Override
+    public void execute() {
+        responsePage="home.jsp";
+        String action = request.getParameter("command").split("\\.")[1];
+        request.getSession().setAttribute("page",action);
+    }
+
+    @Override
+    public String getResponsePage() {
+        return this.responsePage;
+    }
+
+    private Userlp3DAO lookupUserlp3DAOBean() {
+        try {
+            Context c = new InitialContext();
+            return (Userlp3DAO) c.lookup("java:global/BaseLP3/BaseLP3-ejb/Userlp3DAO!com.br.lp3.model.dao.Userlp3DAO");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
+}
